1998bee167ff55045c6a03c49810c690594c3d79
nireo <eemil.lehtikangas1@gmail.com>
2020-02-14 18:01:43 +0200

    changed to npm and added services

---
+++ client/src/services/link.service.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from 'axios';
+const baseUrl = '/api/link';
+
+let token: string | null = null;
+
+const getConfig = () => ({
+  headers: { Authorization: token }
+});
+
+export const setToken = (newToken: string): void => {
+  token = `bearer ${newToken}`;
+};
+
+export const createLink = async (original: string) => {
+  const response = await axios.post(`${baseUrl}/create`, { original });
+  return response.data;
+};
+
+export const deleteLink = async (uuid: string) => {
+  const response = await axios.delete(`${baseUrl}/${uuid}`);
+  return response.data;
+};
